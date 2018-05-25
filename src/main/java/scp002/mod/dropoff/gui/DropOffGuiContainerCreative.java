e9d3f73f45a89c576d9aa6e83a0f99324d098857
SCP002 <feedusfeedme@gmail.com>
2018-05-25 20:47:47 +0300

    Show creative inventory button only if survival inventory tab is open

--- src/main/java/scp002/mod/dropoff/gui/DropOffGuiContainerCreative.java
+++ src/main/java/scp002/mod/dropoff/gui/DropOffGuiContainerCreative.java
@@ -3,6 +3,7 @@ package scp002.mod.dropoff.gui;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainerCreative;
+import net.minecraft.creativetab.CreativeTabs;
 import scp002.mod.dropoff.DropOff;
 import scp002.mod.dropoff.config.DropOffConfig;
 import scp002.mod.dropoff.message.MainMessage;
@@ -42,6 +43,9 @@ class DropOffGuiContainerCreative extends GuiContainerCreative {
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         super.drawScreen(mouseX, mouseY, partialTicks);
+
+        // Make button visible only if survival inventory tab is open.
+        dropOffGuiButton.visible = super.func_147056_g() == CreativeTabs.tabInventory.getTabIndex();
 
         if (dropOffGuiButton.func_146115_a()) { // If the button is hovered by mouse.
             super.drawHoveringText(dropOffGuiButton.hoverText, mouseX, mouseY, super.fontRendererObj);
