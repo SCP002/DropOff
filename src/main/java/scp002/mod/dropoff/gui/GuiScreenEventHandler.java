5d57c1eeffa157dd74707edc530a06a946fb3047
SCP002 <feedusfeedme@gmail.com>
2018-05-27 23:40:58 +0300

    Preparations to add Quark button overriding option, round 2

--- src/main/java/scp002/mod/dropoff/gui/GuiScreenEventHandler.java
+++ src/main/java/scp002/mod/dropoff/gui/GuiScreenEventHandler.java
@@ -1,22 +1,27 @@
 package scp002.mod.dropoff.gui;
 
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.inventory.GuiContainerCreative;
+import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import scp002.mod.dropoff.DropOff;
+import scp002.mod.dropoff.message.MainMessage;
 
 public class GuiScreenEventHandler {
     public static final GuiScreenEventHandler INSTANCE = new GuiScreenEventHandler();
 
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    @SubscribeEvent(priority = EventPriority.HIGH)
     public void onActionPreformed(GuiScreenEvent.ActionPerformedEvent.Pre event) {
-        if (!event.getButton().getClass().getName().equals("vazkii.quark.management.client.gui.GuiButtonChest") ||
+        if (!(event.getGui() instanceof GuiInventory || event.getGui() instanceof GuiContainerCreative) ||
+                !event.getButton().getClass().getName().equals("vazkii.quark.management.client.gui.GuiButtonChest") ||
                 !GuiScreen.isShiftKeyDown()) {
             return;
         }
 
-        // event.setCanceled(true);
+        event.setCanceled(true);
 
-        // ...
+        DropOff.NETWORK.sendToServer(MainMessage.INSTANCE);
     }
 }
