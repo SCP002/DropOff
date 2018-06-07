ac694c341020919e38ad16b3483b1ca9c9b942f6
SCP002 <feedusfeedme@gmail.com>
2018-06-07 14:44:21 +0300

    Rework logger

---
+++ src/main/java/scp002/mod/dropoff/util/LogMessageFactory.java
@@ -0,0 +1,25 @@
+package scp002.mod.dropoff.util;
+
+import org.apache.logging.log4j.message.*;
+import scp002.mod.dropoff.DropOff;
+
+public class LogMessageFactory implements MessageFactory {
+    public static final LogMessageFactory INSTANCE = new LogMessageFactory();
+
+    private final String prefix = "[" + DropOff.MOD_ID + "]: ";
+
+    @Override
+    public Message newMessage(Object message) {
+        return new ObjectMessage(prefix + message);
+    }
+
+    @Override
+    public Message newMessage(String message) {
+        return new SimpleMessage(prefix + message);
+    }
+
+    @Override
+    public Message newMessage(String message, Object... params) {
+        return new ParameterizedMessage(prefix + message, params);
+    }
+}
